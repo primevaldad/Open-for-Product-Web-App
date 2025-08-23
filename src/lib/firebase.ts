88c77a18eee14741451c10964e42a720dc20b95c
David Palmer <primevaldad@gmail.com>
2025-08-23 00:26:58 +0000

    Got an error in new tab preview: Console Error
    FirebaseError: Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  "projectId": "open-for-product",
-  "appId": "1:36569631324:web:9cacdc439f6b8ced253499",
-  "storageBucket": "open-for-product.firebasestorage.app",
-  "apiKey": "AIzaSyCDRVhRRr1rZHUYqWQBzWI_EZHtZ6iWhmY",
-  "authDomain": "open-for-product.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "36569631324"
+  projectId: 'open-for-product',
+  appId: '1:36569631324:web:9cacdc439f6b8ced253499',
+  storageBucket: 'open-for-product.appspot.com',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCDRVhRRr1rZHUYqWQBzWI_EZHtZ6iWhmY",
+  authDomain: 'open-for-product.firebaseapp.com',
+  messagingSenderId: '36569631324',
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
