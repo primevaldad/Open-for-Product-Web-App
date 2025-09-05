f02292aa74119d985501703d533d075402929477
David Palmer <primevaldad@gmail.com>
2025-09-05 03:10:02 +0000

    I did put it in the .env file, but I don't think your instructions befor

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,3 +1,4 @@
+
 import * as admin from 'firebase-admin';
 
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
@@ -6,9 +7,7 @@ if (!serviceAccount) {
     throw new Error('Firebase service account key not found. Please set the FIREBASE_SERVICE_ACCOUNT_KEY environment variable.');
 }
 
-const serviceAccountJson = JSON.parse(
-  Buffer.from(serviceAccount, 'base64').toString('utf8')
-);
+const serviceAccountJson = JSON.parse(serviceAccount);
 
 let adminApp: admin.app.App;
 
