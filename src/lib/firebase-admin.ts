827246cc8f4bd3e4c4bf126fed8c78e1f20dec2c
David Palmer <primevaldad@gmail.com>
2025-09-07 05:04:45 +0000

    Error: "FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-p

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,6 +1,4 @@
 
-'use server';
-
 import { cert, getApp, getApps, initializeApp, type ServiceAccount } from 'firebase-admin/app';
 import serviceAccountJson from '@/lib/serviceAccountKey.json';
 
@@ -15,6 +13,4 @@ if (!getApps().length) {
   adminApp = getApp();
 }
 
-// Do not export adminAuth directly.
-// Instead, other server actions will import adminApp and call getAuth(adminApp).
 export { adminApp };
