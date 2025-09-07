1db947e441166e1d028e3ed68de77e8bdecd95e6
David Palmer <primevaldad@gmail.com>
2025-09-07 05:41:34 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,16 +3,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// IMPORTANT: Replace this with your actual Firebase config object
-// from the Firebase console.
 const firebaseConfig = {
-  "projectId": "open-for-product",
-  "appId": "1:36569631324:web:9cacdc439f6b8ced253499",
-  "storageBucket": "open-for-product.appspot.com",
   "apiKey": "AIzaSyAz_-_rWTApPK5bJAgO93Z5SxqTVT5iDQ",
   "authDomain": "open-for-product.firebaseapp.com",
-  "measurementId": "G-5G01E1N3L7",
-  "messagingSenderId": "36569631324"
+  "projectId": "open-for-product",
+  "storageBucket": "open-for-product.appspot.com",
+  "messagingSenderId": "36569631324",
+  "appId": "1:36569631324:web:9cacdc439f6b8ced253499",
+  "measurementId": "G-5G01E1N3L7"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
