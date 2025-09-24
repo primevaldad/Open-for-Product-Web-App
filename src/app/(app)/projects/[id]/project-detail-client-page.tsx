742579c4d84ca2481cd5292aacd18afb6fb709f8
David Palmer <primevaldad@gmail.com>
2025-09-24 18:59:43 +0000

    fix type in sort (b.timestamp) - (a.timestamp) was b.timestamp - b.timestamp, i.e. always 0

--- src/app/(app)/projects/[id]/project-detail-client-page.tsx
+++ src/app/(app)/projects/[id]/project-detail-client-page.tsx
@@ -325,7 +325,7 @@ export default function ProjectDetailClientPage({
 
                       <div className="space-y-4">
                          {projectDiscussions && projectDiscussions.length > 0 ? (
-                              [...projectDiscussions].sort((a, b) => new Date(b.timestamp).getTime() - new Date(b.timestamp).getTime()).map(comment => (
+                              [...projectDiscussions].sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()).map(comment => (
                               <div key={comment.id} className="flex items-start gap-4">
                                   <Avatar className="h-10 w-10 border">
                                       <AvatarImage src={comment.user.avatarUrl} alt={comment.user.name} />
